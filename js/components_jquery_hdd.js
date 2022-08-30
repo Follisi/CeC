1d11adf5569067508e0aaad80b3b972f2879b7e6
Alexander Follisi <98494280+Follisi@users.noreply.github.com>
2022-08-30 12:56:05 +0200

    CeC

---
+++ js/components_jquery_hdd.js
@@ -0,0 +1,22 @@
+const itemList = jQuery.parseJSON(`{ "hddList" : }`);
+
+var allObjects = itemList.hddList.length;
+var n = 29;
+var m = (allObjects / n) + (allObjects % n);
+
+for (let e = 0; e < allObjects; e++) {
+        $("#itemContainer").append(`
+        <div class="col mb-5 item_shop">
+            <div class="comp-cpu card h-100">
+            <img class="object-img card-img-top" src="" alt="preview non disponibile">
+                <div class="card-body p-4">
+                    <div class="text-center">
+                        <h5 class="object-name fw-bolder"></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `);
+        $('.object-name')[e].innerHTML = itemList.hddList[e].name;
+        $('.object-img')[e].src = itemList.hddList[e].imglink;
+}
---
+++ js/components_jquery_mdue.js
@@ -0,0 +1,22 @@
+const itemList = jQuery.parseJSON(`{ "mdueList" : }`);
+
+var allObjects = itemList.mdueList.length;
+var n = 29;
+var m = (allObjects / n) + (allObjects % n);
+
+for (let e = 0; e < allObjects; e++) {
+        $("#itemContainer").append(`
+        <div class="col mb-5 item_shop">
+            <div class="comp-cpu card h-100">
+            <img class="object-img card-img-top" src="" alt="preview non disponibile">
+                <div class="card-body p-4">
+                    <div class="text-center">
+                        <h5 class="object-name fw-bolder"></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `);
+        $('.object-name')[e].innerHTML = itemList.mdueList[e].name;
+        $('.object-img')[e].src = itemList.mdueList[e].imglink;
+}
